79343b3802edef82ad8da0fcb5a421138d505ab5
unknown <dawidans714@gmail.com>
2023-07-22 14:45:22 +0200

    Imported 4 extra libraries

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import quandl, math
 import numpy as np
-from sklearn import preprocessing
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
