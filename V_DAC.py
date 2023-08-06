5b7e405efa593b09c0f34296e5339d3a2a34fe65
unknown <dawidans714@gmail.com>
2023-08-06 22:14:37 +0200

    Added a comment

--- V_DAC.py
+++ V_DAC.py
@@ -1,3 +1,7 @@
+#That algorithm takes a decimal number representation of a signal and
+#returns the analog voltage level that would be created by a DAC if it were
+#given the same number in binary
+
 def V_DAC(value):
     AVM = value * 5 / 1023
     return round(AVM, 2)
