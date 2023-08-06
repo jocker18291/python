8607da30acf8f55fbed27f4449365883e6586ee1
unknown <dawidans714@gmail.com>
2023-08-06 21:53:39 +0200

    Added a user input for nodes

--- is_adjacent.py
+++ is_adjacent.py
@@ -2,3 +2,13 @@ import numpy as np
 
 def is_adjacent(matrix, node1, node2):
     return 0
+
+matrix = np.array([
+  [ 0, 1, 0, 1, 1 ],
+  [ 1, 0, 1, 0, 0 ],
+  [ 0, 1, 0, 1, 0 ],
+  [ 1, 0, 1, 0, 1 ],
+  [ 1, 0, 0, 1, 0 ]
+])
+node1 = int(input("Enter the first node: "))
+node2 = int(input("Enter the second node: "))
