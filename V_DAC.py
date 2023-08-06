6a7b7ddfe56d634aa9ce7bbcbee9e9ed46583d15
unknown <dawidans714@gmail.com>
2023-08-06 22:12:32 +0200

    Rounding to two decimals added

--- V_DAC.py
+++ V_DAC.py
@@ -1,6 +1,6 @@
 def V_DAC(value):
     AVM = value * 5 / 1023
-    return AVM
+    return round(AVM, 2)
 
 value = float(input("Enter a decimal number representation of a signal: "))
 print("The analog voltage level equals", V_DAC(value))
