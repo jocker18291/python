41045976ceab370ebb4017e8930b55c994352bff
unknown <dawidans714@gmail.com>
2023-08-06 21:56:52 +0200

    Simple bug fixed

--- is_adjacent.py
+++ is_adjacent.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def is_adjacent(matrix, node1, node2):
-    if matrix[node1][node2] == '1':
+    if matrix[node1][node2] == 1:
         return True
     else:
         return False
