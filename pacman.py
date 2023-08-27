5d268783c483e29aa0a4932f9165b8f5b779a912
unknown <dawidans714@gmail.com>
2023-08-27 16:54:48 +0200

    Added a function and a loop for creating a player

--- pacman.py
+++ pacman.py
@@ -11,6 +11,9 @@ timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.image.load(f'player_images/{i}.png'))
 
 def draw_board(level):
     num1 = ((height - 50) // 32)
@@ -35,11 +38,16 @@ def draw_board(level):
                 pygame.draw.arc(screen, 'blue', pygame.Rect(j * num2 - (num2 * 0.5), i * num1 - (0.5 * num1), num2, num1), 1.5 * pi, 0, 3)
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)), ((j + 1) * num2, i * num1 + (0.5 * num1)), 3)
+
+def draw_player():
+    pass 
+
 run = True
 while run:
     timer.tick(fps)
     screen.fill('black')
     draw_board(level)
+    draw_player()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
