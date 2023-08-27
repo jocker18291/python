809b13ca2d640425dc8bff3967ac5869b6baf66e
unknown <dawidans714@gmail.com>
2023-08-27 11:03:57 +0200

    Added top left corners

--- pacman.py
+++ pacman.py
@@ -27,6 +27,8 @@ def draw_board(level):
                 pygame.draw.line(screen, 'blue', (j * num2, i * num1 + (0.5 * num1)), ((j + 1) * num2, i * num1 + (0.5 * num1)), 3)
             if level[i][j] == 5:
                 pygame.draw.arc(screen, 'blue', pygame.Rect(j * num2 - (num2 * 0.5), i * num1 + (0.5 * num1), num2, num1), 0, 0.5 * pi, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, 'blue', pygame.Rect(j * num2 + (num2 * 0.5), i * num1 + (0.5 * num1), num2, num1), 0.5 * pi, pi, 3)
 run = True
 while run:
     timer.tick(fps)
